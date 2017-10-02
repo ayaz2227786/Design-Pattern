65723896c9d147ade5bd980e00063ec27d37213c
ayaz ahamad <ayaz.ahamad@cars24.com>
2017-10-02 15:36:19 +0530

    design patern

---
+++ app/src/main/java/datastructure/example/com/datastructure/syncronise/ExecutionFlow.java
@@ -0,0 +1,15 @@
+package datastructure.example.com.datastructure.syncronise;
+
+/**
+ * Created by ${ayaz} on 9/29/2017.
+ */
+
+public class ExecutionFlow {
+
+    public  static void main(String args[]){
+        System.out.println("print main method");
+    }
+    static {
+        System.out.println("print static block");
+    }
+}
